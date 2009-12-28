c04a313ef405a34c4d7e6f998c585ebb6a6a1f60
Kyle Machulis <kyle@nonpolynomial.com>
2009-12-28 13:31:37 -0800

    Fixed deletion arguments

--- include/nputil_libusb1.h
+++ include/nputil_libusb1.h
@@ -11,6 +11,7 @@
 #define NPUTIL_LIBUSB1_H
 #include "libusb.h"
 
+#ifndef nputil_libusb1_struct
 typedef struct {
 	struct libusb_context* _context;
 	struct libusb_device_handle* _device;
@@ -19,12 +20,13 @@ typedef struct {
 	int _is_open;
 	int _is_inited;
 } nputil_libusb1_struct;
+#endif
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 	nputil_libusb1_struct* nputil_libusb1_create_struct();
-	void nputil_libusb1_delete_struct();
+	void nputil_libusb1_delete_struct(nputil_libusb1_struct* s);
 	int nputil_libusb1_init(nputil_libusb1_struct* s);
 	int nputil_libusb1_count(nputil_libusb1_struct* s, int vendor_id, int product_id);
 	int nputil_libusb1_open(nputil_libusb1_struct* s, int vendor_id, int product_id, int index);
